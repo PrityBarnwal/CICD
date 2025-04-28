274a09f170ad3c11e3528a5362fa7b04a1e00f2a
Prity Barnwal <prity@imerchantech.com>
2025-04-28 10:08:34 +0530

    remove unwanted code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.ci_cd"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.ci_cd"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
